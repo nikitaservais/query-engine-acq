74a2e95b584b154a138d67397c0e4689779b0635
nservais <nikita@lab-box.studio>
2023-12-16 15:04:31 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,201 @@
+mod test;
+
+use std::collections::{HashMap, HashSet};
+use std::fmt;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct Term(String);
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct Atom {
+    relation_name: String,
+    terms: Vec<Term>,
+}
+
+struct Query {
+    _head: Atom,
+    body: Vec<Atom>,
+}
+
+impl Query {
+    fn is_acyclic(&self) -> bool {
+        let hypergraph = Hypergraph::new(self);
+        hypergraph.is_acyclic()
+    }
+
+    fn construct_join_tree(&self) -> Option<JoinTree> {
+        if !self.is_acyclic() {
+            return None;
+        }
+        let mut join_tree = JoinTree::new();
+        let mut hypergraph = Hypergraph::new(self);
+
+        while let Some((ear, witness)) = hypergraph.find_ear() {
+            if ear == witness {
+                hypergraph.hyperedges.remove(&ear);
+                continue;
+            }
+            join_tree.add_edge(ear.clone(), witness.clone());
+            hypergraph.hyperedges.remove(&ear);
+        }
+
+        Some(join_tree)
+    }
+}
+
+#[derive(Clone, Debug)]
+struct JoinTree {
+    edges: HashSet<(Atom, Atom)>,
+}
+
+impl JoinTree {
+    fn new() -> Self {
+        JoinTree { edges: HashSet::new() }
+    }
+
+    fn add_edge(&mut self, ear: Atom, witness: Atom) {
+        self.edges.insert((ear, witness));
+    }
+}
+
+impl fmt::Display for JoinTree {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "JoinTree:\n")?;
+        for (ear, witness) in &self.edges {
+            write!(f, "[")?;
+            let ear_terms: Vec<String> = ear.terms.iter().map(|term| term.0.clone()).collect();
+            write!(f, "{}", ear_terms.join(", "))?;
+            write!(f, "] -> [")?;
+            let witness_terms: Vec<String> = witness.terms.iter().map(|term| term.0.clone()).collect();
+            write!(f, "{}", witness_terms.join(", "))?;
+            write!(f, "]\n")?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Hypergraph {
+    hyperedges: HashMap<Atom, HashSet<Term>>,
+}
+
+impl Hypergraph {
+    fn new(query: &Query) -> Self {
+        let mut edges = HashMap::new();
+        for atom in &query.body {
+            let variables = atom.terms.iter().cloned().collect();
+            edges.insert(atom.clone(), variables);
+        }
+        Hypergraph { hyperedges: edges }
+    }
+
+
+    fn find_ear(&self) -> Option<(Atom, Atom)> {
+        for (ear_candidate, ear_vertices) in &self.hyperedges {
+            // all vertices of the hyperedge are exclusive to that hyperedge
+            if self.is_vertices_exclusive(ear_candidate, ear_vertices) {
+                return Some((ear_candidate.clone(), ear_candidate.clone()));
+            }
+            // there exists another hyperedge w (called a witness of ear_candidate) such that every vertices in ear_candidate is either exclusive to ear_candidate or also occurring in witness
+            let mut exclusive_vertices = HashSet::new();
+            let mut witness_vertices = HashSet::new();
+            for vertex in ear_vertices {
+                if self.is_vertex_exclusive(ear_candidate, vertex) {
+                    exclusive_vertices.insert(vertex.clone());
+                } else {
+                    witness_vertices.insert(vertex.clone());
+                }
+            }
+            for (witness_candidate, witness_candidate_vertices) in &self.hyperedges {
+                if ear_candidate != witness_candidate {
+                    if witness_vertices.is_subset(witness_candidate_vertices) {
+                        return Some((ear_candidate.clone(), witness_candidate.clone()));
+                    }
+                }
+            }
+        }
+        None
+    }
+
+    fn is_acyclic(&self) -> bool {
+        match self.find_ear() {
+            // If an ear is found, remove it and check the remaining hypergraph
+            Some((ear, _)) => {
+                let mut remaining_hypergraph = self.clone();
+                remaining_hypergraph.hyperedges.remove(&ear);
+                remaining_hypergraph.is_acyclic()
+            }
+            // If no ear is found, terminate and check the hypergraph is empty
+            None => {
+                self.hyperedges.is_empty()
+            }
+        }
+    }
+
+    fn is_vertices_exclusive(&self, hyperedge: &Atom, vertices: &HashSet<Term>) -> bool {
+        for (other_hyperedge, other_vertices) in &self.hyperedges {
+            if hyperedge != other_hyperedge {
+                for vertex in vertices {
+                    if other_vertices.contains(vertex) {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+
+    fn is_vertex_exclusive(&self, hyperedge: &Atom, vertex: &Term) -> bool {
+        for (other_hyperedge, other_vertices) in &self.hyperedges {
+            if hyperedge != other_hyperedge {
+                if other_vertices.contains(vertex) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+// Continue with the Query and JoinTree structs and associated methods...
+fn main() {
+    let head = Atom {
+        relation_name: "Answer".to_string(),
+        terms: vec![],
+    };
+
+    let body = vec![
+        Atom {
+            relation_name: "Beers".to_string(),
+            terms: vec![
+                Term("beer_id".to_string()),
+                Term("brew_id".to_string()),
+                Term("beer".to_string()),
+                Term("abv".to_string()),
+                Term("ibu".to_string()),
+                Term("ounces".to_string()),
+                Term("style".to_string()),
+                Term("style2".to_string()),
+            ],
+        },
+        Atom {
+            relation_name: "Styles".to_string(),
+            terms: vec![
+                Term("style_id".to_string()),
+                Term("cat_id".to_string()),
+                Term("style".to_string()),
+            ],
+        },
+        Atom {
+            relation_name: "Categories".to_string(),
+            terms: vec![
+                Term("cat_id".to_string()),
+                Term("‘Belgian and French Ale’".to_string()),
+            ],
+        },
+    ];
+
+    let query = Query { _head: head, body };
+    let join_tree = query.construct_join_tree().unwrap();
+    println!("{}", join_tree)
+}
